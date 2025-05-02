bbf59d7bbfab0a8990b43fe21a2d9feba0cc5c42
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-05-02 16:37:18 +0530

    fixed some linting issues

--- service/routes.py
+++ service/routes.py
@@ -115,13 +115,12 @@ def update_accounts(account_id):
 
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-def delete_accounts(account_id):
-    app.logger.info("Request to delete an Account with id: %s", account_id)
+def delete_account(account_id):
     account = Account.find(account_id)
     if account:
         account.delete()
-        return "", status.HTTP_204_NO_CONTENT
-    return "", status.HTTP_404_NOT_FOUND
+        return '', status.HTTP_204_NO_CONTENT
+    return '', status.HTTP_404_NOT_FOUND
 
 
 ######################################################################
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,12 +138,12 @@ class TestAccountService(TestCase):
     def test_update_account(self):
         test_account = AccountFactory()
         resp = self.client.post(BASE_URL, json=test_account.serialize())
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
 
         new_account = resp.get_json()
         new_account["name"] = "Thathsara"
         resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=test_account.serialize())
-        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Thathsara")
 
